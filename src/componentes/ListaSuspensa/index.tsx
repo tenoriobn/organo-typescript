410e624e1de2f6587e467c061d60630e50d40f43
Bruno Tenório <tenoriobn@gmail.com>
2023-08-30 13:57:43 -0300

    Migrando Lista Suspensa

--- src/componentes/ListaSuspensa/index.tsx
+++ src/componentes/ListaSuspensa/index.tsx
@@ -1,6 +1,15 @@
 import './ListaSuspensa.css'
 
-const ListaSuspensa = (props) => {
+interface ListaSuspensaProps {
+    aoAlterado: (valor: string) => void
+    label: string
+    required: boolean
+    valor: string
+    itens: string[]
+
+}
+
+const ListaSuspensa = (props: ListaSuspensaProps) => {
     return (
         <div className='lista-suspensa'>
             <label>{props.label}</label>
